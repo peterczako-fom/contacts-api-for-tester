9f503a6e5580e01b4e87266635bfca24790a9792
CzakoPeter <peter.czako@fututreofmedia.com>
2022-05-10 11:41:57 +0200

    remove

--- src/main/java/hu/futureofmedia/task/contactsapi/configuration/JpaAuditingConfiguration.java
+++
@@ -1,9 +0,0 @@
-package hu.futureofmedia.task.contactsapi.configuration;
-
-import org.springframework.context.annotation.Configuration;
-import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
-
-@Configuration
-@EnableJpaAuditing
-public class JpaAuditingConfiguration {
-}

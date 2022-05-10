85fea95c49e9ab0ac718bd3fc90c75fdc6f44d75
CzakoPeter <peter.czako@fututreofmedia.com>
2022-05-10 11:29:28 +0200

    string for dates and companyName -> companyId

--- src/main/java/hu/futureofmedia/task/contactsapi/dto/ContactDto.java
+++ src/main/java/hu/futureofmedia/task/contactsapi/dto/ContactDto.java
@@ -6,9 +6,6 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
-import java.time.LocalDateTime;
-import java.util.Objects;
-
 @NoArgsConstructor
 @AllArgsConstructor
 @Getter
@@ -30,40 +27,12 @@ public class ContactDto {
     @Schema(example = "36201234567")
     private String phoneNumber;
 
-    @Schema(example = "Company #1")
-    private String companyName;
+    @Schema(example = "1")
+    private Long companyId;
 
     @Schema(example = "Some comment for contact")
     private String comment;
-    private LocalDateTime createdDate;
-    private LocalDateTime lastUpdatedDate;
+    private String createdDate;
+    private String lastModifiedDate;
 
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-        ContactDto that = (ContactDto) o;
-        return id.equals(that.id) && firstName.equals(that.firstName) && lastName.equals(that.lastName) && email.equals(that.email) && Objects.equals(phoneNumber, that.phoneNumber) && companyName.equals(that.companyName) && comment.equals(that.comment) && createdDate.equals(that.createdDate) && lastUpdatedDate.equals(that.lastUpdatedDate);
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(id, firstName, lastName, email, phoneNumber, companyName, comment, createdDate, lastUpdatedDate);
-    }
-
-    @Override
-    public String toString() {
-        return "ContactInfo{" +
-                "id=" + id +
-                ", firstName='" + firstName + '\'' +
-                ", lastName='" + lastName + '\'' +
-                ", email='" + email + '\'' +
-                ", phoneNumber='" + phoneNumber + '\'' +
-                ", companyName='" + companyName + '\'' +
-                ", comment='" + comment + '\'' +
-                ", createdDate=" + createdDate +
-                ", lastUpdatedDate=" + lastUpdatedDate +
-                '}';
-    }
 }

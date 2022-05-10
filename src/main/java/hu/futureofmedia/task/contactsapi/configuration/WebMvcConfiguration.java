9b5217fb4b01c5168bd7139eb9ca4640a8ab2cd4
CzakoPeter <peter.czako@fututreofmedia.com>
2022-05-10 11:27:17 +0200

    add custom corsMapping and interceptor

---
+++ src/main/java/hu/futureofmedia/task/contactsapi/configuration/WebMvcConfiguration.java
@@ -0,0 +1,25 @@
+package hu.futureofmedia.task.contactsapi.configuration;
+
+import hu.futureofmedia.task.contactsapi.interceptors.LoggerInterceptor;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+@EnableWebMvc
+public class WebMvcConfiguration implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("http://localhost:4200/")
+                .allowedMethods("GET", "POST", "PUT", "DELETE");
+    }
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(new LoggerInterceptor());
+    }
+}

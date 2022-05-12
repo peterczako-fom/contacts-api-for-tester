1436898df70c4714d2839bea4515263cf3d2bd20
CzakoPeter <peter.czako@fututreofmedia.com>
2022-05-12 12:15:48 +0200

    remove expressions and add entity to details dto

--- src/main/java/hu/futureofmedia/task/contactsapi/mapper/ContactMapper.java
+++ src/main/java/hu/futureofmedia/task/contactsapi/mapper/ContactMapper.java
@@ -1,31 +1,37 @@
 package hu.futureofmedia.task.contactsapi.mapper;
 
 import hu.futureofmedia.task.contactsapi.dto.ContactCreateAndUpdateDto;
+import hu.futureofmedia.task.contactsapi.dto.ContactDetailsDto;
 import hu.futureofmedia.task.contactsapi.dto.ContactDto;
 import hu.futureofmedia.task.contactsapi.dto.ContactListItemDto;
 import hu.futureofmedia.task.contactsapi.entities.Contact;
-import hu.futureofmedia.task.contactsapi.service.CompanyService;
 import org.mapstruct.*;
-import org.springframework.beans.factory.annotation.Autowired;
 
 @Mapper(componentModel = "spring")
-public abstract class ContactMapper {
+public interface ContactMapper {
 
-    @Autowired
-    public CompanyService companyService;
-
-    @Mapping(target = "company", expression = "java(companyService.findCompanyById(dto.getCompanyId()))")
-    public abstract Contact dtoToEntityForCreate(ContactCreateAndUpdateDto dto);
+    @Mapping(target = "companyName", source = "company.name")
+    ContactListItemDto entityToListItemDto(Contact entity);
 
     @Mapping(target = "companyId", source = "company.id")
-    @Mapping(target = "createdDate", source = "createdDate", dateFormat = "YYYY-MM-dd HH:mm:ss Z")
-    @Mapping(target = "lastModifiedDate", source = "lastModifiedDate", dateFormat = "YYYY-MM-dd HH:mm:ss Z")
-    public abstract ContactDto entityToDto(Contact entity);
+    ContactDto entityToDto(Contact entity);
 
-    @Mapping(target = "fullName", expression = "java(entity.getFirstName() + \" \" + entity.getLastName())")
     @Mapping(target = "companyName", source = "company.name")
-    public abstract ContactListItemDto entityToListItemDto(Contact entity);
+    ContactDetailsDto entityToDetailsDto(Contact entity);
 
-    @Mapping(target = "company", expression = "java(companyService.findCompanyById(dto.getCompanyId()))")
-    public abstract void updateContact(@MappingTarget Contact entity, ContactCreateAndUpdateDto dto);
+    @Mapping(target = "id", ignore = true)
+    @Mapping(target = "company", ignore = true)
+    @Mapping(target = "status", ignore = true)
+    @Mapping(target = "createdDate", ignore = true)
+    @Mapping(target = "lastModifiedDate", ignore = true)
+    Contact dtoToEntityForCreate(ContactCreateAndUpdateDto dto);
+
+    @Mapping(target = "id", ignore = true)
+    @Mapping(target = "company", ignore = true)
+    @Mapping(target = "status", ignore = true)
+    @Mapping(target = "createdDate", ignore = true)
+    @Mapping(target = "lastModifiedDate", ignore = true)
+    void updateContact(@MappingTarget Contact entity, ContactCreateAndUpdateDto dto);
+
+
 }

90a6942ed80513affc6a0464f79f109e69407d8d
CzakoPeter <peter.czako@fututreofmedia.com>
2022-05-12 12:16:40 +0200

    get all active contact and modify status

--- src/main/java/hu/futureofmedia/task/contactsapi/repositories/ContactRepository.java
+++ src/main/java/hu/futureofmedia/task/contactsapi/repositories/ContactRepository.java
@@ -1,20 +1,19 @@
 package hu.futureofmedia.task.contactsapi.repositories;
 
-import hu.futureofmedia.task.contactsapi.dto.ContactListItemDto;
 import hu.futureofmedia.task.contactsapi.entities.Contact;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 
 public interface ContactRepository extends JpaRepository<Contact, Long> {
 
-    @Query("select new hu.futureofmedia.task.contactsapi.dto.ContactListItemDto(" +
-            "c.id, concat(c.firstName, ' ', c.lastName), c.email, c.phoneNumber, c.company.name) from Contact c " +
-            "where c.status = :status")
-    Page<ContactListItemDto> getAllActiveContact(Pageable pageable, Contact.Status status);
+    @Query("select c from Contact c where c.status = :status")
+    Page<Contact> getAllContactByStatus(Pageable pageable, Contact.Status status);
 
-    default Page<ContactListItemDto> getAllActiveContact(Pageable pageable) {
-        return getAllActiveContact(pageable, Contact.Status.ACTIVE);
-    }
+    @Modifying
+    @Query("update Contact c set c.status = :status where c.id = :id")
+    void updateStatusById(Long id, Contact.Status status);
+
 }

748f3b93eff4263062673855ae81ee3c45b823e5
CzakoPeter <peter.czako@fututreofmedia.com>
2022-05-12 12:17:13 +0200

    remove enableWebMvc and logging interception

--- src/main/java/hu/futureofmedia/task/contactsapi/configuration/WebMvcConfiguration.java
+++ src/main/java/hu/futureofmedia/task/contactsapi/configuration/WebMvcConfiguration.java
@@ -1,25 +1,21 @@
 package hu.futureofmedia.task.contactsapi.configuration;
 
-import hu.futureofmedia.task.contactsapi.interceptors.LoggerInterceptor;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.EnableWebMvc;
-import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
-@EnableWebMvc
 public class WebMvcConfiguration implements WebMvcConfigurer {
 
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
                 .allowedOrigins("http://localhost:4200/")
-                .allowedMethods("GET", "POST", "PUT", "DELETE");
-    }
-
-    @Override
-    public void addInterceptors(InterceptorRegistry registry) {
-        registry.addInterceptor(new LoggerInterceptor());
+                .allowedMethods(
+                        HttpMethod.GET.name(),
+                        HttpMethod.POST.name(),
+                        HttpMethod.PUT.name(),
+                        HttpMethod.DELETE.name());
     }
 }
